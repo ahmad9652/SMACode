5108621068e95116de0b57009d595a40bb2d1a8e
ahmad9652 <ahmadseemapuri@gmail.com>
2023-08-19 18:07:06 +0530

    Project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { QuestionPage } from './modules/ide/pages/QuestionPage';
+
+function App() {
+  return (
+    <QuestionPage/>
+  );
+}
+
+export default App;
---
+++ frontend/src/modules/ide/pages/QuestionPage.jsx
@@ -0,0 +1,3 @@
+export const QuestionPage = () => {
+    return (<h1>Question Page</h1>)
+}
